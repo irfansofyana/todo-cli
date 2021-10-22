987eef29f6f68908eecbdf171eb2088947fb1c4f
irfansofyana <irfansofyana0305@gmail.com>
2021-10-22 23:35:05 +0700

    chore: add install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#! /bin/bash -
+
+# install all library dependencies
+pip install -r requirements.txt
+
+# create database file
+databasePath="./src/database.db"
+echo "creating database..."
+if [[ ! -f "$databasePath" ]]; then
+    touch $databasePath
+    echo "database created!"
+else
+    echo "database is already exist!"
+fi
+
+# Build the application into binary file
+cd src
+pyinstaller --add-data "database.db:." --add-data "../pyfiglet:./pyfiglet" todo-cli.py --onefile
